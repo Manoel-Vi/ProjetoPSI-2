c0b23647989f9ff98f0e38c9186bc926eb7bcc63
Manoel Victor <medeiros.manoel@escolar.ifrn.edu.br>
2026-07-14 13:43:05 -0300

    feat: adiciona rota cadastro inicial
    altera a rota index também

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Rota inicial"
+    return render_template('index.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_temlate('cadastro.html')
