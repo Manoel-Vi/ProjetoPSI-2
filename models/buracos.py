f73c2d4ae03608e15c0197eee469e386ddfe6fe7
Manoel <medeiros.manoel@escolar.ifrn.edu.br>
2026-07-16 09:30:58 -0300

    impr: adiciona arquivo modelo da tabela buracos

---
+++ models/buracos.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+
+database = SQLAlchemy()
+
+class Buraco(database.Model):
+    __tablename__ = "buracos"
+
+    id = database.Column(database.Integer, primary_key=True)
+    rua = database.Column(database.String(150), nullable=False)
+    bairro = database.Column(database.String(100), nullable=False)
+    cidade = database.Column(database.String(100), nullable=False)
+    gravidade = database.Column(database.String(20), nullable=False)
+
+    usuario_id = database.Column(
+        database.Integer,
+        database.ForeignKey("usuarios.id"),
+        nullable=False
+    )
+
+    def __repr__(self):
+        return f'<{self.rua}>'
